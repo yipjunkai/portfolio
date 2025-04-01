526286e88def177846d7588166f73749c3c7df7f
Yip Jun Kai <yipjunkai@gmail.com>
2025-04-02 04:40:33 +0800

    Add Projects page component with initial content and styling

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,13 @@
+export default function Projects() {
+  return (
+    <div className="space-y-8">
+      <h1 className="text-4xl font-bold">Projects</h1>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
+        assumenda eum tempore reprehenderit libero asperiores rerum odio
+        quibusdam deserunt, est repellat ducimus commodi nemo tenetur similique
+        iure esse explicabo unde.
+      </div>
+    </div>
+  );
+}
