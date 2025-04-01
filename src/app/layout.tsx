fe50ee7bf51b47bbddffeef0fee0c46b7a494148
Yip Jun Kai <yipjunkai@gmail.com>
2025-04-01 18:12:15 +0800

    Implement mobile top navigation and refactor layout structure for improved responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import Link from "next/link";
+import { GithubIcon } from "@/components/icons/GithubIcon";
+import { LinkedinIcon } from "@/components/icons/LinkedinIcon";
+import MobileTopNav from "@/components/nav/MobileTopNav";
 import {
-  HomeIcon,
   BriefcaseIcon,
   CodeBracketIcon,
+  HomeIcon,
 } from "@heroicons/react/24/outline";
-import { LinkedinIcon } from "@/components/icons/LinkedinIcon";
-import { GithubIcon } from "@/components/icons/GithubIcon";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
+import { JSX } from "react";
+import "./globals.css";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -29,26 +32,14 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="absolute left-1/2 w-1/2 h-full bg-neutral-900 z-0"></div>
-        <div
-          className="absolute left-1/2 -translate-x-1/2
-         flex flex-row h-screen w-full max-w-7xl mx-auto z-10 *:h-full"
-        >
-          <Sidebar />
-          <main className="grow bg-neutral-900">{children}</main>
-        </div>
-      </body>
-    </html>
-  );
-}
-
-const Sidebar = () => {
-  const sections = [
+  const sections: {
+    name: string;
+    routes: {
+      name: string;
+      href: string;
+      icon: JSX.Element;
+    }[];
+  }[] = [
     {
       name: "About Me",
       routes: [
@@ -87,28 +78,57 @@ const Sidebar = () => {
   ];
 
   return (
-    <div className="w-48 p-4">
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <div className="hidden lg:block absolute left-1/2 w-1/2 h-full bg-neutral-900 z-0"></div>
+        <div
+          className="absolute left-1/2 -translate-x-1/2
+         flex flex-col lg:flex-row h-screen w-full max-w-7xl mx-auto z-10 *:lg:h-full"
+        >
+          <Sidebar sections={sections} />
+          <MobileTopNav sections={sections} />
+          <main className="grow bg-neutral-900">{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
+
+const Sidebar = (props: {
+  sections: {
+    name: string;
+    routes: {
+      name: string;
+      href: string;
+      icon: JSX.Element;
+    }[];
+  }[];
+}) => {
+  return (
+    <nav className="w-48 p-4 hidden lg:flex flex-col">
       <input
         type="text"
         placeholder="Search"
         className="rounded-md bg-neutral-800 py-1.5 px-4 w-[calc(100%+0.5rem)] -ml-2"
       />
       <div className="flex flex-col gap-2 mt-4">
-        {sections.map((route) => (
-          <div key={route.name} className="not-first:mt-4">
+        {props.sections.map((section) => (
+          <div key={section.name} className="not-first:mt-4">
             <h2 className="text-xs font-bold uppercase text-neutral-600">
-              {route.name}
+              {section.name}
             </h2>
             <div className="flex flex-col gap-2 py-2">
-              {route.routes.map((r) => (
+              {section.routes.map((routes) => (
                 <Link
-                  href={r.href}
-                  key={r.name}
+                  href={routes.href}
+                  key={routes.name}
                   className="hover:bg-neutral-800 p-2 -ml-2 rounded-md capitalize flex flex-row gap-4 justify-start items-center group transition-all *:transition-all duration-200 *:duration-200"
                 >
-                  {r.icon}
+                  {routes.icon}
                   <span className="group-hover:text-white text-neutral-300">
-                    {r.name}
+                    {routes.name}
                   </span>
                 </Link>
               ))}
@@ -116,6 +136,6 @@ const Sidebar = () => {
           </div>
         ))}
       </div>
-    </div>
+    </nav>
   );
 };
---
+++ src/components/nav/MobileTopNav.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import Image from "next/image";
+import Link from "next/link";
+import { JSX, useState } from "react";
+
+export default function MobileTopNav(props: {
+  sections: {
+    name: string;
+    routes: {
+      name: string;
+      href: string;
+      icon: JSX.Element;
+    }[];
+  }[];
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const flatRoutes = props.sections.flatMap((section) => section.routes);
+
+  return (
+    <div className="relative">
+      <nav className="w-full p-4 pl-6 flex flex-row lg:hidden justify-between items-center">
+        <Image src="/favicon.ico" alt="logo" width={28} height={28} />
+
+        <div className="flex flex-row gap-2 *:p-2">
+          {/* Search button */}
+          <button>
+            <MagnifyingGlassIcon className="w-6 h-6" />
+          </button>
+
+          <div className="w-10"></div>
+        </div>
+      </nav>
+      {/* Menu button */}
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="absolute top-4 right-4 group flex h-10 w-10 cursor-pointer items-center justify-center z-10 lg:hidden"
+      >
+        <div className="space-y-2">
+          <span
+            className={`block h-1 w-6 origin-center rounded-full bg-neutral-500 transition-transform ease-in-out ${isOpen ? "translate-y-1.5 rotate-45" : ""}`}
+          ></span>
+          <span
+            className={`block h-1 w-4 origin-center rounded-full from-purple-700 to-rose-600 bg-linear-to-r transition-transform ease-in-out ${isOpen ? "w-6 -translate-y-1.5 -rotate-45" : ""}`}
+          ></span>
+        </div>
+      </button>
+      <div
+        className={`absolute top-0 left-0 w-full h-screen bg-background transition-all duration-500  ${
+          isOpen ? "translate-y-0 opacity-100" : "-translate-y-full opacity-0"
+        }`}
+      >
+        <div className="flex flex-col gap-6 p-4">
+          {flatRoutes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.name}
+              className="capitalize text-4xl first:mt-16 pl-8 last:mb-48"
+            >
+              {route.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
