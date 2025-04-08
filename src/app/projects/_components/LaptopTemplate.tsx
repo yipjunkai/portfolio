4796c4027b6632b84c950184427a148c1bb49238
Yip Jun Kai <yipjunkai@gmail.com>
2025-04-08 15:58:39 +0800

    Lighter

--- src/app/projects/_components/LaptopTemplate.tsx
+++ src/app/projects/_components/LaptopTemplate.tsx
@@ -7,13 +7,13 @@ export default function LaptopTemplate({
 }) {
   return (
     <div className={`relative ${className}`}>
-      <div className="relative mx-auto border-gray-800 dark:border-gray-800 bg-gray-800 border-[8px] rounded-t-xl w-[95%] aspect-[512/294]">
-        <div className="rounded-lg overflow-hidden h-full w-full bg-white dark:bg-gray-800 *:h-full *:w-full *:rounded-lg *:bg-white *:dark:bg-gray-600">
+      <div className="relative mx-auto border-gray-700 dark:border-gray-700 bg-gray-700 border-[8px] rounded-t-xl w-[95%] aspect-[512/294]">
+        <div className="rounded-lg overflow-hidden h-full w-full bg-white dark:bg-gray-700">
           {children}
         </div>
       </div>
-      <div className="relative mx-auto bg-gray-900 dark:bg-gray-700 rounded-b-xl rounded-t-sm w-full aspect-[597/21]">
-        <div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl w-[16%] aspect-[96/8] bg-gray-800"></div>
+      <div className="relative mx-auto bg-gray-900 dark:bg-gray-600 rounded-b-xl rounded-t-sm w-full aspect-[597/21]">
+        <div className="absolute left-1/2 top-0 -translate-x-1/2 rounded-b-xl w-[16%] aspect-[96/8] bg-gray-700"></div>
       </div>
     </div>
   );
