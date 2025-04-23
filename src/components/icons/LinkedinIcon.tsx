7f56b6780457270511dcb3bfc4ce07f47df7891f
Yip Jun Kai <yipjunkai@gmail.com>
2025-04-23 15:39:46 +0800

    Refactor icon components to use default export: change GithubIcon and LinkedinIcon to default exports for consistency and improved import syntax.

--- src/components/icons/LinkedinIcon.tsx
+++ src/components/icons/LinkedinIcon.tsx
@@ -1,6 +1,6 @@
 import { SVGProps } from "react";
 
-export function LinkedinIcon(props: SVGProps<SVGSVGElement>) {
+export default function LinkedinIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
