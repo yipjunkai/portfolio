9d33f11493d81424d91c633cf65db77e0900af0a
Jun Kai <yipjunkai@gmail.com>
2026-01-31 18:03:13 -0800

    Add ESLint and Prettier configurations: introduce .prettierrc for code formatting rules, update eslint.config.mjs to define comprehensive ESLint configurations with TypeScript and Tailwind CSS support, and modify package.json scripts for linting and formatting tasks.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,92 @@
+// @ts-check
+import { defineConfig } from "eslint/config";
+import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
+import eslintPluginNext from "@next/eslint-plugin-next";
+
+import tsEslint from "typescript-eslint";
+import globals from "globals";
+import js from "@eslint/js";
+import unusedImports from "eslint-plugin-unused-imports";
+import eslintConfigPrettier from "eslint-config-prettier";
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+const configs = defineConfig(
+  {
+    ignores: ["node_modules", "dist", ".next", "**/*.d.ts"]
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    extends: [js.configs.recommended, tsEslint.configs.disableTypeChecked]
+  },
+  {
+    files: ["**/*.{ts,tsx,spec.ts,spec.tsx}"],
+    extends: [tsEslint.configs.recommended, tsEslint.configs.stylistic]
+  },
+  {
+    plugins: {
+      "unused-imports": unusedImports,
+      "@typescript-eslint": tsEslint.plugin
+    },
+    rules: {
+      "max-len": [
+        "error",
+        {
+          code: 120,
+          ignoreComments: true,
+          ignoreUrls: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true
+        }
+      ],
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+          caughtErrors: "none",
+          destructuredArrayIgnorePattern: "^_"
+        }
+      ]
+    }
+  },
+  {
+    ignores: ["messages", "public"]
+  },
+  {
+    plugins: {
+      tailwindcss: eslintPluginTailwindcss,
+      "@next/next": eslintPluginNext
+    },
+    rules: {
+      ...eslintPluginNext.configs.recommended.rules,
+      ...eslintPluginNext.configs["core-web-vitals"].rules,
+      ...eslintPluginTailwindcss.configs.recommended.rules
+    },
+    settings: {
+      tailwindcss: {
+        config: `${__dirname}/src/app/globals.css`
+      }
+    }
+  },
+  eslintConfigPrettier
+);
 
-export default eslintConfig;
+export default configs;
