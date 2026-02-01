8892cdc9fd52f76a430a4546adafb68618d3b80e
Jun Kai <yipjunkai@gmail.com>
2026-01-31 19:52:57 -0800

    Update routing configuration to limit supported locales to "en-SG" for streamlined internationalization.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,6 +2,6 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   localePrefix: "as-needed",
-  locales: ["en-SG", "en-US", "zh-CN", "zh-SG"],
+  locales: ["en-SG"],
   defaultLocale: "en-SG"
 });
