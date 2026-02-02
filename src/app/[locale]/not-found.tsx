87866aad4cd18537fa5962964b813df90b0d147b
Jun Kai <yipjunkai@gmail.com>
2026-02-01 16:47:32 -0800

    Add localization support for 404 Not Found page: update en-SG.json to include not-found messages and modify NotFound component to utilize translations for improved user experience.

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -3,16 +3,19 @@ import { HomeIcon } from "@heroicons/react/24/solid";
 import undrawVibeCoding from "./_assets/undraw_vibe-coding_mjme.svg";
 import { Link } from "@/i18n/navigation";
 import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
 
 export default function NotFound() {
+  const t = useTranslations("not-found");
+
   return (
     <div className="flex h-4/5 flex-col items-center justify-center space-y-4">
-      <h1 className="text-4xl font-bold">Page Not Found</h1>
-      <Image src={undrawVibeCoding} alt="404 page illustration" className="size-160" />
-      <Button variant="default" asChild>
+      <h1 className="text-4xl font-bold">{t("title")}</h1>
+      <Image src={undrawVibeCoding} alt={t("illustration-alt")} className="size-60" />
+      <Button variant="default" asChild className="mt-32 md:mt-20">
         <Link href="/">
           <HomeIcon className="size-6" />
-          <span>Go home</span>
+          <span>{t("go-home")}</span>
         </Link>
       </Button>
     </div>
