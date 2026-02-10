f52eb10f1c829b8ec8535be9e77438fb45d90de0
Jun Kai <yipjunkai@gmail.com>
2026-02-10 03:21:33 -0800

    Update resume URL in config.ts for accuracy

--- src/config.ts
+++ src/config.ts
@@ -7,6 +7,6 @@ export const siteConfig = {
   },
   resume: {
     filename: "YipJunKai_CV.pdf",
-    url: "https://umpsbusvwgpktceb.public.blob.vercel-storage.com/resume_EN-SVX0krPLPxukHyrkKyzStsEBJvyxt5.pdf"
+    url: "https://umpsbusvwgpktceb.public.blob.vercel-storage.com/resume_EN-e5CcwujcbTgcDdJC09oPqWTvxq8P2X.pdf"
   }
 };
