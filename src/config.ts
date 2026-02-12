141be6cfe68254f82edee5c0fcff9cacd15d893d
Jun Kai <yipjunkai@gmail.com>
2026-02-12 03:52:11 -0800

    Update terminology in en-SG.json and config.ts: Change "CV" to "Resume" for consistency across the application.

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const siteConfig = {
     linkedin: "https://www.linkedin.com/in/yipjk/"
   },
   resume: {
-    filename: "YipJunKai_CV.pdf",
+    filename: "YipJunKai_Resume.pdf",
     url: "https://umpsbusvwgpktceb.public.blob.vercel-storage.com/resume_EN_SG-w2uRfbhhgEHKDL9QpUU58p2JoAI8eC.pdf"
   }
 };
