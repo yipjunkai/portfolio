7f1ef5913750f670744cd3503171dfbb68075ddd
Jun Kai <yipjunkai@gmail.com>
2026-04-23 18:54:20 -0700

    Refactor Sidebar component imports
    Consolidates the import statements in the Sidebar component by adding the usePathname hook from the navigation module, improving code organization and readability.

--- src/app/[locale]/_components/Sidebar.tsx
+++ src/app/[locale]/_components/Sidebar.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import Image from "next/image";
-import { Link } from "@/i18n/navigation";
+import { Link, usePathname } from "@/i18n/navigation";
 import ThemeChanger from "./ThemeChanger";
 import LanguageChanger from "./LanguageChanger";
 import { ExternalLinkIcon } from "lucide-react";
